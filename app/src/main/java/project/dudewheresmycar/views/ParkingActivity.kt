c3a16a5af73f87531b9e32158372c75be50a6198
Sharjeel Ahmed <msharjeel.irshad@gmail.com>
2020-10-02 20:56:30 +1000

    Completed the following layouts: - activity_parking - activity_reminder

---
+++ app/src/main/java/project/dudewheresmycar/views/ParkingActivity.kt
@@ -0,0 +1,12 @@
+package project.dudewheresmycar.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.dudewheresmycar.R
+
+class ParkingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parking)
+    }
+}
---
+++ app/src/main/java/project/dudewheresmycar/views/ReminderActivity.kt
@@ -0,0 +1,12 @@
+package project.dudewheresmycar.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.dudewheresmycar.R
+
+class ReminderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reminder)
+    }
+}
