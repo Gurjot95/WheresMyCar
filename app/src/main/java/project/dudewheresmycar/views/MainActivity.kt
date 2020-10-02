2a1f8c7e969112c6e6db8d789335e57200855e06
Sharjeel Ahmed <msharjeel.irshad@gmail.com>
2020-10-02 20:56:08 +1000

    Completed the following layouts: - activity_parking - activity_reminder

--- app/src/main/java/project/dudewheresmycar/views/MainActivity.kt
+++ app/src/main/java/project/dudewheresmycar/views/MainActivity.kt
@@ -1,12 +1,16 @@
 package project.dudewheresmycar.views
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import project.dudewheresmycar.R
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
