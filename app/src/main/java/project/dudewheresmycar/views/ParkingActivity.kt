6d794d9e9798e8932323b0fcbce150e9a16a772e
Sharjeel Ahmed <msharjeel.irshad@gmail.com>
2020-10-12 21:19:38 +1100

    Added MapView to ParkingActivity

--- app/src/main/java/project/dudewheresmycar/views/ParkingActivity.kt
+++ app/src/main/java/project/dudewheresmycar/views/ParkingActivity.kt
@@ -1,28 +1,38 @@
 package project.dudewheresmycar.views
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.toolbar.view.*
 import project.dudewheresmycar.R
 import project.dudewheresmycar.databinding.ActivityParkingBinding
 import project.dudewheresmycar.viewmodel.ParkingActivityViewModel
 
-class ParkingActivity : AppCompatActivity() {
+
+class ParkingActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var viewModel: ParkingActivityViewModel
     private lateinit var binding: ActivityParkingBinding
+
+    private var mapView: MapView? = null
+    private var googleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         viewModel = ViewModelProviders.of(this).get(ParkingActivityViewModel::class.java)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_parking)
+
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync(this)
 
         binding.yesButton.setOnClickListener {
             addParking()
@@ -33,9 +43,12 @@ class ParkingActivity : AppCompatActivity() {
         }
 
         // Toolbar modifications
-        binding.homeToolbar.toolbar.background = ContextCompat.getDrawable(this,R.drawable.semi_circle_cyan)
+        binding.homeToolbar.toolbar.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.semi_circle_cyan
+        )
         binding.homeToolbar.toolbarLogo.setImageResource(R.drawable.ic_traffic)
-        binding.homeToolbar.toolbarLogo.setColorFilter(ContextCompat.getColor(this,R.color.white))
+        binding.homeToolbar.toolbarLogo.setColorFilter(ContextCompat.getColor(this, R.color.white))
         binding.homeToolbar.toolbarTitle.text = resources.getString(R.string.parking_title)
         binding.homeToolbar.toolbarDesc.text = resources.getString(R.string.parking_info)
     }
@@ -47,5 +60,16 @@ class ParkingActivity : AppCompatActivity() {
         // Display map
         // Set map marker for parking location
         // Save all parking data to shared preferences
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap?.apply {
+            val sydney = LatLng(-33.852, 151.211)
+            addMarker(
+                MarkerOptions()
+                    .position(sydney)
+                    .title("Marker in Sydney")
+            )
+        }
     }
 }
